d6048bfaedc86405dab15f83d1e91f516e0feac3
MuhammadHatta72 <attah1335@gmail.com>
2026-01-29 10:56:13 +0700

    refactor: update button components and navigation layout for improved styling and functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link rel="stylesheet" href="https://cdn.datatables.net/2.3.6/css/dataTables.dataTables.css" />
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
@@ -32,5 +33,10 @@
                 {{ $slot }}
             </main>
         </div>
+
+        <!-- Global Scripts -->
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/2.3.6/js/dataTables.min.js"></script>
+        @stack('scripts')
     </body>
 </html>
